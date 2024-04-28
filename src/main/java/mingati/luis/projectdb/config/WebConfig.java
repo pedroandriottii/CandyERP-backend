d45344ba764465c1ae0f824b09f24914c895bdea
Pedro Andriotti <phab@cesar.school>
2024-04-28 16:35:52 -0300

    Fixing allowedOrigins

--- src/main/java/mingati/luis/projectdb/config/WebConfig.java
+++ src/main/java/mingati/luis/projectdb/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000", "https://candyerp.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
